67d5bcdee3e452dc1ed77370e5acbd8bd74f3a31
Celebrimbor02 <gibson8fla@gmail.com>
2023-09-08 18:51:52 -0300

    Problema de Cors resolvido.

--- src/main/java/com/empresa/projeto/StartApplication.java
+++ src/main/java/com/empresa/projeto/StartApplication.java
@@ -1,9 +1,12 @@
 package com.empresa.projeto;
 
 import com.empresa.projeto.entity.Cliente;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(servers = {@Server(url = "/", description = "URL PADRAO")})
 @SpringBootApplication
 public class StartApplication {
 
